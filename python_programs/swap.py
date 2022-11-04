7e35ab58aa01dd075dafaf4374aa469df1aaca30
RAMRATAN SHARMA <77084946+nittratan@users.noreply.github.com>
2022-11-04 16:26:40 +0530

    practice programs

---
+++ python_programs/swap.py
@@ -0,0 +1,8 @@
+# swap two numbers
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("Before swapping a=",a,"b=",b)
+c=a
+a=b
+b=c
+print("After swapping a=",a,"b=",b)
